182d44625d48988f3a24e82b353af6a77685fcea
avendanoangeline77 <uba.avendaoangelina@gmail.com>
2024-04-22 13:58:37 -0300

    Arrancando con js

---
+++ javaScript/primero.js
@@ -0,0 +1,11 @@
+var numero = 1;//asigna el valor a una variable tipo numero
+var palabra = "palabra"
+
+
+//definimos variables con la palabra reservada let para evitar ser llamada en cualquier lado de codigo 
+let numero2 = 2;
+let palabra2 = "palabra 2"; 
+
+
+//definimos constante con la variable const 
+const numConstante  = 4; //siempre que sepan que no van a mutar 
