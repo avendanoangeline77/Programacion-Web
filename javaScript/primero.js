ace7ff19b301fb26ca18947a6bf3b13bd08e7483
avendanoangeline77 <uba.avendanoangelina@gmail.com>
2024-04-22 16:55:46 -0300

    termine los ejercicios

--- javaScript/primero.js
+++ javaScript/primero.js
@@ -9,3 +9,12 @@ let palabra2 = "palabra 2";
 
 //definimos constante con la variable const 
 const numConstante  = 4; //siempre que sepan que no van a mutar 
+
+//ejemplo:
+
+if(numero > 0){
+    let numero3 = 2
+}
+
+console.log("numero", numero2); //mostrar un valor por pantalla
+///////////////////////////////////////////////
